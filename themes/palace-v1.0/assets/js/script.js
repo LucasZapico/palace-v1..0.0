218e2eeb0a6092bf3c57f78fe6b212aaa6daee47
Lucas Zapico <zapicolucas@gmail.com>
2019-08-09 18:59:34 -0700

    remove unused script

--- themes/palace-v1.0/assets/js/script.js
+++ themes/palace-v1.0/assets/js/script.js
@@ -4,14 +4,4 @@ const toggle = document.getElementById('toggle')
 
 toggle.addEventListener("click", e => {
   console.log('click')
-  if(sideNav.classList.contains("show")){
-    sideNav.classList.remove("show");
-    toggle.classList.add("open");
-  }else {
-    sideNav.classList.add("show");
-    toggle.classList.remove("open");
-  }
 })
-
-
-
